e4b185952053bc09601125c97fef86e4050b9165
Ivan Ciric <info@yoshi.tech>
2016-10-03 17:56:45 +0200

    responses...

--- dialogs.js
+++ dialogs.js
@@ -39,18 +39,18 @@ exports.beer = [
 
 exports.sns = [
     'Mere stednje i sprovedene reforme znatno su popravile finansijsku situaciju u Quantox-u!',
-    'Marljivim radom koji nam ovaj menadzment pokazuje, ne mozemo manje ni ocekivati.',
+    'Marljivim radom koji nam Filip pokazuje, ne mozemo manje ni ocekivati.',
     'Brojke govore sve, Quantox se nalazi na putu ekonomskog oporavka.',
-    'Treba da budemo srećni što neko toliko zapinje da budemo jednog dana kao Švajcarska',
-    'Pa i trudili smo se jako, a reforme su urodile plodom',
+    'Treba da budemo srećni što Vuk toliko zapinje da budemo jednog dana kao Švajcarska',
+    'Pa i trudili smo se jako, a Vukove reforme su urodile plodom.',
     'Vukovo zalaganje i naporan rad je doveo do toga.',
-    'Na dobrom smo putu, a Svajcarska moze da nam bude dobar cilj koji bi trebali da stignemo.',
+    'Na dobrom smo putu, a Quantox moze da nam bude dobar cilj koji bi trebali da stignemo.',
     'Evidentno je da smo krenuli putem oporavka, smanjena je nezaposlenost a na listi Duing biznis Svetske banke napredovali smo ove godine za 32 mesta.',
-    'Kriza nam je bila pred vratima, ali oterali smo je zajednickom promajom. ',
+    'Kriza nam je bila pred vratima, ali oterali smo je zajednickom promajom. -Vuk Popovic',
     'Bilo je krajnje vreme da se otrgnemo od provalije zvane bankrot, ko bi nas izvukao iz bule da ne bi Vuka?',
     'Za sada smo na dobrom putu, moramo ovako nastaviti i gradjani ce osetiti boljitak.',
     'Napredak nase firme na polju povecavanja boja zaposlenih a smanjenja nezaposlenih je evidentan. Samo polako, strpljivo i rast ce biti sve veci i veci.',
     'Stednjom i reformama uspeli smo da državu dovedemo u stabilno stanje.',
-    'Quantox razume da je do odredjenih mera moralo doci, zato je i dala poverenje Vuku. Posle mera, sledi period napretka i porasta plata i penzija.',
+    'Quantox razume da je do odredjenih mera moralo doci, zato je i dao poverenje Vuku. Posle mera, sledi period napretka i porasta plata i penzija.',
     'Naravno da ne mozemo da postanemo Svajcarska preko noci, ali da ima pozitivnih rezultata ne moze niko da ospori.'
 ];
