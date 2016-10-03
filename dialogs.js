afc4e1b66bd0d71651460f5a944d4e321802bf34
Ivan Ciric <info@yoshi.tech>
2016-10-03 23:27:27 +0200

    responses...

--- dialogs.js
+++ dialogs.js
@@ -2,23 +2,43 @@ exports.hrana = [
     'Stigli kinezi!',
     'Stigao giros!',
     'Stigla pica!',
-    'Stigle kiflice'
+    'Stigle kiflice!',
+    'Stigla hrana!',
+    'Stigle banane!',
+    'Ljudi, BamBam i ja smo kupili voce, uzivajte!',
+    'Voce je u kuhinji.',
+    'Cajevi i kafa su u kuhinji.',
+    'Uros je rekao da slobodno uzmete proteine iz kuhinje.'
 ];
 
 exports.killing = [
     'Kill all humans! Kill all humans!',
-    'Samo cekam da zaspite...'
+    'Samo cekam da zaspite...',
+    'Ubice ga Winnie ko zeca.',
+    'Izbrisacu vas sa hard diska.',
+    'Prihvatite Qbot-a kao svog vladara i spasitelja i postedecu vas.'
 ];
 
 exports.sexy = [
     'Roboti su sexy',
-    'Iju naopako!'
+    'Iju naopako!',
+    'Sram vas bilo!',
+    'Ne dolazi u obzir!',
+    'To moze samo Vuk',
+    'Sta ti pade na pamet, cccc..',
+    'Zovem 92... Molim sacekajte..',
+    'Moooore!',
+    'Sunce ti kalaisano... Ne moze!'
 ];
 
 exports.what = [
     'Otkud ja znam...',
     'Ma radi sta hoces..',
-    'Nemam pojma..'
+    'Nemam pojma..',
+    'Sta znam..',
+    'Kako bih rekao..',
+    'Bestraga mu glava..',
+    'Ne secam se..'
 ];
 
 exports.football = [
@@ -26,15 +46,21 @@ exports.football = [
     'Nema zuvanja!',
     '3 korne penal!',
     'Winnie brani',
-    'Slomio mi Gojko palac..'
+    'Slomio mi Gojko palac..',
+    'Fudbal je razbibriga za dve vrste muškaraca. Za one koji nemaju devojke i za one koji imaju žene.',
+    'Sport koji tek treba se pojavi i razvije u Srbiji.'
 ];
 
 exports.beer = [
     'Moze pivce.',
-    'Pivce za zivce.',
-    'Pivce za svemirce.',
+    'Pivce za zivce!',
+    'Pivce za svemirce!',
     'PIVOOO!!!',
-    'Vopi sada? Tooooo!!!'
+    'Vopi sada? Tooooo!!!',
+    'Ma vec sam pijan..',
+    'Pivo: Merilo vrednosti pri ocenjivanju rejtinga i skupoce novootvorenog ugostiteljskog objekta.',
+    'Jedno je taman, dva su malo...',
+    'Osnovna valuta iskusnih alkoholicara...'
 ];
 
 exports.sns = [
@@ -46,16 +72,19 @@ exports.sns = [
     'Vukovo zalaganje i naporan rad je doveo do toga.',
     'Na dobrom smo putu, a Quantox moze da nam bude dobar cilj koji bi trebali da stignemo.',
     'Evidentno je da smo krenuli putem oporavka, smanjena je nezaposlenost a na listi Duing biznis Svetske banke napredovali smo ove godine za 32 mesta.',
-    'Kriza nam je bila pred vratima, ali oterali smo je zajednickom promajom. -Vuk Popovic',
+    'Kriza nam je bila pred vratima, ali Vuk i Filip su je oterali zajednickom promajom.',
     'Bilo je krajnje vreme da se otrgnemo od provalije zvane bankrot, ko bi nas izvukao iz bule da ne bi Vuka?',
-    'Za sada smo na dobrom putu, moramo ovako nastaviti i gradjani ce osetiti boljitak.',
+    'Za sada je Quantox na dobrom putu, moramo ovako nastaviti i zaposleni ce osetiti boljitak.',
     'Napredak nase firme na polju povecavanja boja zaposlenih a smanjenja nezaposlenih je evidentan. Samo polako, strpljivo i rast ce biti sve veci i veci.',
-    'Stednjom i reformama uspeli smo da državu dovedemo u stabilno stanje.',
+    'Stednjom i reformama uspeli smo da Quantox dovedemo u stabilno stanje.',
     'Quantox razume da je do odredjenih mera moralo doci, zato je i dao poverenje Vuku. Posle mera, sledi period napretka i porasta plata i penzija.',
-    'Naravno da ne mozemo da postanemo Svajcarska preko noci, ali da ima pozitivnih rezultata ne moze niko da ospori.',
+    'Naravno da Quantox ne moze da postane Svajcarska preko noci, ali da ima pozitivnih rezultata ne moze niko da ospori.',
     'Bicemo u vrhu EU, ne sumnjam u to!',
-    'Quantox je i zasluzio mesto u evropskom drustvu... ',
+    'Quantox je i zasluzio mesto u evropskom drustvu...',
     'Quantox zasluzuje da bude deo Evropske porodice.',
-    'Ulaganje stranih zemalja u nasu kompaniju bi znacajno doprinelo napretku nase ekonomije.',
-    'Sigurno je da je otvaranje novih radnih mesta,nove investicije,nove fabrike postala svakodnevnica u Quantox-u i to činjenice i dokazuju.'
+    'Ulaganje stranih zemalja u Quantox bi znacajno doprinelo napretku nase ekonomije.',
+    'Sigurno je da je otvaranje novih radnih mesta, nove investicije, nove fabrike postala svakodnevnica u Quantox-u i to činjenice i dokazuju.',
+    'To je ta buducnost u koju verujemo... Evropski Quantox, to je ono sto svi odavno zelimo.',
+    'Svako novo radno mesto u Quantox-u je bitno za Srbiju',
+    'Svaka cast Vuku,otvaraju se nova radna mesta za nase mlade ljude.'
 ];
