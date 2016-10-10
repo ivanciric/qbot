905b71f2daba04854d46b85eef883faac149f0a5
Dan <danijel.petrovic@cosmicdevelopment.com>
2016-10-10 17:58:21 +0200

    separate environment variables from main code

---
+++ env.js
@@ -0,0 +1,6 @@
+exports.environment = 'DEVELOPMENT';
+
+exports.appId       = '122402f6-b3c1-483f-80b6-4fcb98e0caed';
+exports.appPassword = 'kSLLNRm5SUh27EJ9RCA7bZO';
+exports.key         = '/etc/apache2/ssl/yoshi.key';
+exports.certificate = '/etc/apache2/ssl/yoshi.crt';
