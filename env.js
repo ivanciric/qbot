cadf7b221955d961092a49b6aca0173dfcb55c44
Ivan Ciric <info@yoshi.tech>
2016-10-11 17:31:48 +0200

    Merge branch 'positive-negative-relationships' of https://github.com/TribalKing/qbot into TribalKing-positive-negative-relationships
    # Conflicts:
    #	dialogs.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 exports.environment = 'DEVELOPMENT';
 
-exports.appId       = '100dd9b7-18e7-4e8f-a51c-ca400c3224e4';
-exports.appPassword = '9LuZnctwpfpvhGfGACNrFi7';
+exports.appId       = '';
+exports.appPassword = '';
 exports.key         = '';
 exports.certificate = '';
