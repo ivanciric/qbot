b0eccb150ee320513b7ba31bc23a9ea6d77d2018
Ivan Ciric <info@yoshi.tech>
2017-07-20 14:56:54 +0200

    Adds AC climate control

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var Intelligence = require("./intelligence");
 var Relationships = require("./relationships");
 var Transliterator = require("./transliterate");
 
+const io    = require('socket.io-client');
+
 var serverOptions = {};
 
 if(env.environment != 'DEVELOPMENT')
@@ -122,6 +124,35 @@ bot.dialog('/', function (session) {
         });
     }
 
+    /**
+     * Klima control
+     *
+     */
+    var klimaData = text.match(/(klima) (.*) (.*)/i);
+
+    if(klimaData && klimaData.length > 0)
+    {
+        var klimaName = klimaData[2];
+        var klimaCommand = klimaData[3];
+
+        var klimaHost = env.klimas[klimaName];
+
+        if(klimaCommand == 'power'){
+            var socket  = io.connect(klimaHost, { reconnect: true });
+            socket.emit('key_power', {
+                remote_name: 'klimax',
+                command: 'KEY_POWER',
+                user_id: 'someuser',
+                timestamp: new Date()
+            });
+            session.send( 'Komanda "' + klimaCommand + '" prosledjena na klimu br. ' + klimaName);
+        }else{
+            session.send( '"' + klimaCommand + '" nije validna komanda. Foooo! (try power)');
+        }
+
+
+    }
+
     if(text.contains('memes'))
     {
         return meme.list(function(memes){
@@ -129,4 +160,5 @@ bot.dialog('/', function (session) {
             session.send( memes );
         });
     }
+
 });
