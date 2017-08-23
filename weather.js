71e4bb83db1f437e2710a2ca7dc37c0be6ec4e27
Ivan <ivan.djordjevic@quantox.com>
2017-08-23 14:04:41 +0200

    Weather info for subotica and kragujevac

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ function weather() {}
 
 weather.prototype.fetchTemp = function(callback) {
 
-    var urlVreme = "http://api.openweathermap.org/data/2.5/group?id=791148,792078" +
+    var urlVreme = "http://api.openweathermap.org/data/2.5/group?id=791148,792078,3189595,789128 " +
         "&appid=8468ccd3277f4d4a4b8f815a3b2bd756&units=metric";
 
     request({
@@ -20,6 +20,10 @@ weather.prototype.fetchTemp = function(callback) {
 
             var weatherCacak = '';
 
+            var weatherSubotica = '';
+
+            var weatherKragujevac = '';
+
             body.list.forEach(function (item) {
 
                 if( item.id == '791148' ){// Belgrade
@@ -29,10 +33,22 @@ weather.prototype.fetchTemp = function(callback) {
                 if( item.id == '792078' ){// Cacak
                     weatherCacak = item.main;
                 }
+
+                if( item.id == '3189595' ){// Subotica
+                    weatherSubotica = item.main;
+                }
+
+                if( item.id == '789128' ){// Kragujevac
+                    weatherKragujevac = item.main;
+                }
+
+
+
             });
 
             callback('Beograd: ' + parseFloat(weatherBelgrade.temp).toFixed(1) + ' °C\n\nČačak: ' +
-                parseFloat(weatherCacak.temp).toFixed(1) + ' °C');
+                parseFloat(weatherCacak.temp).toFixed(1) + ' °C\n\nKragujevac: ' + parseFloat(weatherKragujevac.temp).toFixed(1) + ' °C\n\nSubotica: ' +
+                parseFloat(weatherSubotica.temp).toFixed(1));
         }
     });
 };
